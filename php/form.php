75849c574dd2c6fec339507a236c22b04092cb56
BaptisteRoyer <56670984+BaptisteRoyer@users.noreply.github.com>
2021-10-12 11:47:26 +0200

    Initial commit

---
+++ php/form.php
@@ -0,0 +1,26 @@
+<?php
+
+if(isset($_POST['submit_btn'])){
+ 	$fmin = $_POST['fmin'];
+	$fmax = $_POST['fmax'];
+	$nbpts = $_POST['nbpts'];
+	$dap = $_POST['dap'];
+	$spar = $_POST['spar'];
+	$mesureType = $_POST['mesureType'];
+
+	$text = $fmin . "," . $fmax . "," . $nbpts . "," . $dap . "," . $spar . "," . $mesureType;
+	$fp = fopen('data.csv', 'w');
+
+	if(fwrite($fp, $text))  {
+	    echo 'saved';
+
+	}
+	else{
+		echo 'didnt work';
+	}
+	fclose ($fp);    
+
+	header( 'Location: ../graphs.html' );
+}
+
+?>
