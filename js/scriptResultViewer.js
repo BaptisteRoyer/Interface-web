27939aa7121b9a542a6fb4a45dce070375653749
BaptisteRoyer <56670984+BaptisteRoyer@users.noreply.github.com>
2022-02-16 15:51:15 +0100

    Changed IP

--- js/scriptResultViewer.js
+++ js/scriptResultViewer.js
@@ -1,6 +1,6 @@
 var mqtt;
 var reconnectTimeout = 2000;
-var host = "127.0.0.1"; //change this according to your brokers IP
+var host = "192.158.20.154"; //change this according to your brokers IP
 var port = 9001;
 
 var isConnected = false;
